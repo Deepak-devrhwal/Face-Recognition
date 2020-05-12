553c7810eb1e9fd37859a837bd75a101de7abcd2
Deepak Devrhwal <deepakdevrhwal@gmail.com>
2020-05-13 04:53:59 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
+import Particles from "react-particles-js";
 import "./App.css";
-
+const op = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 900,
+      },
+    },
+  },
+};
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Particles className='particles' params={op} />
         <Navigation />
         <Logo />
+        <Rank />
+        <ImageLinkForm />
         {/* { <Logo/>
-      <ImageLinkForm/>
-      <FaceRecognition/>} */}
+        <FaceRecognition/>} */}
       </div>
     );
   }
